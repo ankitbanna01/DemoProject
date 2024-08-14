edc6a12315542e7e50d9ddc7bdb2215f5d0f5584
ANKIT MANDLOI <ANKIT MANDLOI@DESKTOP-3468QJK>
2024-08-14 23:02:52 +0530

    New My Update

--- Demoo/src/main/java/Ankit/Demoo/App.java
+++ Demoo/src/main/java/Ankit/Demoo/App.java
@@ -8,8 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello");
-        System.out.println("Hii");
-        System.out.println("NOOOOO");
+		/*
+		 * System.out.println("Hello"); System.out.println("Hii");
+		 * System.out.println("NOOOOO");
+		 */
     }
 }
